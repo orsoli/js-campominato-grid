eebbf3da0a002edf1ceef32ff1e6053e78593239
orsoli <fljcoli@gmail.com>
2024-08-23 15:41:26 +0200

    Added milestone 1.Preapare HTML and CSS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-console.log("js ok");
+/* Exercise
+    The user clicks on a button that will generate a square game grid.
+    Each cell has a progressive number, from 1 to 100.
+    Therefore, there will be 10 cells for each of the 10 rows.
+    When the user clicks on any cell, the clicked cell turns blue. 
+    And a message with the number of the clicked cell is printed in the console.
+*/
+
+/* Structure
+    M1 - Preapare the HTML and CSS we see in img.
+*/
